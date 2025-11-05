fb20131143f40c85ad502845afadb07c81924c13
Lin1ux <liniuxpl2@gmail.com>
2025-11-05 19:54:05 +0100

    add camera modes

--- tower defence inz/Assets/Scripts/Camera/CameraController.cs
+++ tower defence inz/Assets/Scripts/Camera/CameraController.cs
@@ -4,14 +4,28 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
+
+    [Header("Dynamic camera position")] [SerializeField]
+    private bool allowDynamicMovement = true;
     [SerializeField] Vector2 displacementMultiplayer = new Vector2(0.15f,0.3f);
 
     void Update()
     {
-        SetCameraPosition();
+        if (allowDynamicMovement)
+        {
+            SetCameraPositionBasedOnMousePosition();
+            return;
+        }
+        SetCameraPostionBasedOnPlayerPosition();
     }
-    
-    void SetCameraPosition()
+
+    //Set Camera position based on attached transform;
+    void SetCameraPostionBasedOnPlayerPosition()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10f);
+    }
+    //Set Camera positon based diffrence between mouse position and attached transform (camera would move toward mouse position)
+    void SetCameraPositionBasedOnMousePosition()
     {
         Vector3 worldMousePosition  = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector3 cameraDisplacement = (worldMousePosition - playerTransform.position) * displacementMultiplayer;
@@ -20,7 +34,14 @@ public class CameraController : MonoBehaviour
         finalCameraPosition.z = -10f;
         transform.position = finalCameraPosition;
     }
+
+    //Change mode of camera
+    public void SetDynamicCameraMovement(bool active)
+    {
+        allowDynamicMovement = active;
+    }
     
+    //Validation
     void OnValidate()
     {
         if (playerTransform == null)
