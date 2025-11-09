02c2bb15cf1014e6398b65cd79caca51a3cdcc69
Krzysztof Mańczak <krzysztfmanczak@gmail.com>
2025-11-09 17:58:10 +0100

    Folder place

---
+++ tower defence inz/Assets/TDPG/EffectSystem/ElementRegistry/Registry.cs
@@ -0,0 +1,20 @@
+using TDPG.EffectSystem.Element;
+// using FastGraph;
+using UnityEngine;
+
+namespace TDPG.EffectSystem.EffectRegistry
+{
+    // public class Registry
+    // {
+    //     // The effect graph storing relationships between effect elements
+    //     private AdjacencyGraph<Element, Edge<Element>> graph;
+    //
+    //     private bool shouldMutate = false;
+    //
+    //     public Registry()
+    //     {
+    //         // Initialize the graph
+    //         graph = new AdjacencyGraph<Element, Edge<Element>>(true);
+    //     }
+    // }
+}
